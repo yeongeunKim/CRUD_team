3d02071b9e2b6a1d385e9b3d66f5b4eeab434cc2
Kim eunjin <21900174@handong.edu>
2020-05-05 00:09:59 +0900

    Update manager.c

---
+++ manager.c
@@ -0,0 +1,19 @@
+#include "manager.h"
+
+void updateCourse(Course *s){
+    printf("과목명은?");
+    getchar();
+    scanf("%[^\n]s",s->courseName);
+    printf("교수님 성함은?");
+    scanf("%[^\n]s",s->proName);
+    printf("전공 또는 교양과목? ");
+    scanf("%[^\n]s",s->major);
+    printf("학점수?");
+    scanf("%[^\n]s",s->credit);
+}
+
+int deleteCourse(Course *s){
+    s->credit=-1;
+    printf("=>삭제됨!\n");
+    return 1;
+}
