b26c14c49245d45c8fa98ca478f80be8441f3643
Kim YeongEun <sally4386@naver.com>
2020-05-04 22:23:03 +0900

    add additional func

--- manager.h
+++ manager.h
@@ -2,8 +2,9 @@
 #include<string.h>
 
 typedef struct{
-	char courseName[30];
-	char proName[30];
+	char courseName[30];//과목명 저장하는 함수
+	char proName[30];//교수님 성함 저장하는 변수
+	char major[20];//교양인지 전공인지 구분하는 변수
 	int credit;
 } Course;
 
@@ -15,3 +16,4 @@ void listCourse(Course *s,int count);
 int selectDataNo(Course *s,int count);
 void saveData(Course *s, int count);
 void searchProfessor(Course *s, int count);
+void searchMajor(Course *s , int count);
