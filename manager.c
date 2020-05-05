947ba63582cdcfd5ff17ac36a420c1748f2165a0
Kim eunjin <21900174@handong.edu>
2020-05-05 18:43:13 +0900

    Update manager.c

--- manager.c
+++ manager.c
@@ -17,3 +17,24 @@ int deleteCourse(Course *s){
     printf("=>삭제됨!\n");
     return 1;
 }
+void searchName(Course s[], int count){
+    int scount=0;
+    char search[20];
+
+    printf("검색할 과목명?");
+    getchar();
+    scanf("%[^\n]s",search);
+
+    for(int i=0; i<count;i++){
+        if(s[i].credit!=-1){
+            if(strstr(s[i].courseName,search))
+            {
+                printf("%2d",i+1 );
+                readCourse(s[i]);
+                scount++;
+            }
+        }
+    }
+    if(scount==0)printf("=>검색된 데이터 없음!");
+    printf("\n");
+}
